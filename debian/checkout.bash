298bd629a5668d978849bb6524dafafae07de1d0
Erik Bernhardson <ebernhardson@wikimedia.org>
2024-10-09 13:27:23 -0700

    Migrate package to opensearch
    To start getting our various test environments going with
    opensearch it would be useful to have a docker image with at
    least some of our plugins available. The docker image uses the
    plugins .deb to ensure it has the same set of plugins we see in
    production without duplicating. As such, migrate this debian
    package over to opensearch to be used there.
    
    Opensearch changes signature verification to .sig files instead
    of .asc files. Appears to work as expected by changing the file
    extension.
    
    This contains all plugins that were available for elasticsearch,
    nothing should be lost in the shift to opensearch.
    
    Bug: T372769
    Change-Id: I50a8f0ea5085b9f8b0c3627932f198aef0ed00ea

--- debian/checkout.bash
+++ debian/checkout.bash
@@ -3,7 +3,7 @@
 
 ROOT_DIR=$1
 PLUGIN_LIST=$2
-ELASTICSEARCH_VERSION=$3
+OPENSEARCH_VERSION=$3
 
 temp_dir=$(mktemp -d)
 
@@ -13,12 +13,14 @@ trap "{ rm -f $LOCKFILE; }" EXIT
 for plugin_def in `cat $PLUGIN_LIST | grep -v '^#'`; do
     rm -rf "${temp_dir}/plugin"
     rm -rf "${temp_dir}/plugin.zip"
-    url="$(echo $plugin_def | cut -d',' -f1 | sed -e 's/$ELASTICSEARCH_VERSION/'$ELASTICSEARCH_VERSION/g )"
+    url="$(echo $plugin_def | cut -d',' -f1 | sed -e 's/$OPENSEARCH_VERSION/'$OPENSEARCH_VERSION/g )"
     echo "Downloading $url ..."
     curl -fLo ${temp_dir}/plugin.zip $url
     key=$(echo $plugin_def | cut -d',' -f2)
     if [ $key != 'none' ]; then
-        curl -fLo "${temp_dir}/plugin.zip.asc" "$url.asc"
+        if ! curl -fLo "${temp_dir}/plugin.zip.asc" "$url.asc"; then
+            curl -fLo "${temp_dir}/plugin.zip.asc" "$url.sig"
+        fi
         rm -f "${temp_dir}/keyring.gpg"
         echo "Verifying gpg signature..."
         gpg --keyserver 'hkp://keyserver.ubuntu.com:11371' --no-default-keyring --keyring "${temp_dir}/keyring.gpg" --recv-keys "${key}"
@@ -27,34 +29,34 @@ for plugin_def in `cat $PLUGIN_LIST | grep -v '^#'`; do
         echo "WARNING trying to download $url without a signature file"
     fi
     unzip "${temp_dir}/plugin.zip" -d "${temp_dir}/plugin"
-    if [ ! -d "${temp_dir}/plugin/elasticsearch" ]; then
+    if [ ! -d "${temp_dir}/plugin/opensearch" ]; then
         rm -f "${temp_dir}/broken_plugin" && \
         mv "${temp_dir}/plugin" "${temp_dir}/broken_plugin" && \
         mkdir -p "${temp_dir}/plugin/" && \
-        mv "${temp_dir}/broken_plugin" "${temp_dir}/plugin/elasticsearch" && \
+        mv "${temp_dir}/broken_plugin" "${temp_dir}/plugin/opensearch" && \
         echo "Fixed broken plugin: ${url}" || \
         echo "WARNING broken plugin detected : ${url}, please investigate"
     fi
 
-    plugin_desc=${temp_dir}/plugin/elasticsearch/plugin-descriptor.properties
+    plugin_desc=${temp_dir}/plugin/opensearch/plugin-descriptor.properties
     if [ ! -f "$plugin_desc" ]; then
         echo "${plugin_desc} File not found, plugin broken?"
         exit 1
     fi
-    plugin_es_version=`grep '^elasticsearch.version=' ${plugin_desc}`
-    plugin_es_version="${plugin_es_version##*=}"
+    plugin_opensearch_version=`grep '^opensearch.version=' ${plugin_desc}`
+    plugin_opensearch_version="${plugin_opensearch_version##*=}"
     plugin_version=`grep '^version=' ${plugin_desc}`
     plugin_version="${plugin_version##*=}"
     plugin_name=`grep '^name=' ${plugin_desc}`
     plugin_name="${plugin_name##*=}"
-    echo "Found matching es version ${plugin_es_version}"
-    if [ "${plugin_es_version}" != "${ELASTICSEARCH_VERSION}" ]; then
-      echo "${plugin_name} does not match elastic version ${ELASTICSEARCH_VERSION}"
+    echo "Found matching opensearch version ${plugin_opensearch_version}"
+    if [ "${plugin_opensearch_version}" != "${OPENSEARCH_VERSION}" ]; then
+      echo "${plugin_name} does not match opensearch version ${OPENSEARCH_VERSION}"
       exit 1
     fi
     mkdir -p "${temp_dir}/deploy/${plugin_name}"
-    cp -a "${temp_dir}/plugin/elasticsearch/." "${temp_dir}/deploy/${plugin_name}"
+    cp -a "${temp_dir}/plugin/opensearch/." "${temp_dir}/deploy/${plugin_name}"
 done
 
-mkdir -p "${ROOT_DIR}/usr/share/elasticsearch/plugins/"
-cp -a "${temp_dir}"/deploy/* "${ROOT_DIR}/usr/share/elasticsearch/plugins/"
+mkdir -p "${ROOT_DIR}/usr/share/opensearch/plugins/"
+cp -a "${temp_dir}"/deploy/* "${ROOT_DIR}/usr/share/opensearch/plugins/"
